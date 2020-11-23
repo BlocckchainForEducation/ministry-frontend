f16de73e574b40ce3572547fb8617c201b7400ea
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-11-23 20:39:18 +0700

    :sparkles: new History view

--- src/views/VoteHistory/index.js
+++ src/views/VoteHistory/index.js
@@ -1,58 +1,48 @@
-import { Box, Button, Divider, Typography } from "@material-ui/core";
-import { useDispatch, useSelector } from "react-redux";
-import { pushFetchedVotedRequests } from "../Voting/redux";
+import { Box, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import View from "../../components/utils/View";
 import VotingRequest from "../Voting/VoteRequest";
 
 export default function VoteHistory(props) {
-  const shouldShowHistory = useSelector((state) => state.votingViewSlice.shouldShowHistory);
-  const votedRequests = useSelector((state) => state.votingViewSlice.votedRequests);
-  const dp = useDispatch();
+  const [state, setState] = useState({
+    loading: true,
+    votedRequests: [],
+  });
+
+  useEffect(() => {
+    fetchVoteHistory();
+  }, []);
 
   async function fetchVoteHistory() {
     const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/voting/vote-requests?state=old`);
     if (response.ok) {
       const voted = await response.json();
-      dp(pushFetchedVotedRequests(voted));
+      setState({ loading: false, votedRequests: voted });
     } else {
       console.log(await response.json());
     }
   }
 
-  const title = (
-    <div>
-      <Divider></Divider>
+  let content;
+  if (state.votedRequests.length > 0) {
+    content = state.votedRequests.map((voted, index) => (
+      <Box mb={3}>
+        <VotingRequest request={voted} key={index}></VotingRequest>
+      </Box>
+    ));
+  } else {
+    content = (
       <Box py={2} mb={3} bgcolor="white">
         <Typography variant="h4" align="center">
-          Lịch sử các lần vote
+          Không có lịch sử vote nào!
         </Typography>
       </Box>
-    </div>
-  );
-
-  const history = votedRequests.map((voted, index) => (
-    <Box mb={3}>
-      <VotingRequest request={voted} key={index}></VotingRequest>
-    </Box>
-  ));
-
-  let content = (
-    <>
-      {title}
-      {history}
-    </>
-  );
+    );
+  }
 
   return (
     <div>
-      {!shouldShowHistory ? (
-        <Box textAlign="right">
-          <Button color="primary" onClick={() => fetchVoteHistory()}>
-            Hiển thị lịch sử
-          </Button>
-        </Box>
-      ) : (
-        content
-      )}
+      <View title="Lịch sử vote">{state.loading ? "loading" : content}</View>
     </div>
   );
 }
