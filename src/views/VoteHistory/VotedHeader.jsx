35020e8f87539f30ea12e56545f16d019629d1fa
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-11-23 22:42:27 +0700

    :zap: /vote-history: improve ui add icon

--- src/views/VoteHistory/VotedHeader.jsx
+++ src/views/VoteHistory/VotedHeader.jsx
@@ -1,6 +1,8 @@
-import { Avatar, Box, Typography } from "@material-ui/core";
-
+import { Avatar, Box, Grid, Typography, useTheme } from "@material-ui/core";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
 export default function VotedHeader({ request }) {
+  const theme = useTheme();
   return (
     <Box bgcolor="white" px={2} py={1} display="flex" alignItems="center">
       <Box flexGrow={1} display="flex" alignItems="center">
@@ -9,7 +11,18 @@ export default function VotedHeader({ request }) {
           <Typography variant="h5">{request.universityName}</Typography>
         </Box>
       </Box>
-      <Box>{request.state === "accepted" ? <i>Đã chấp nhận</i> : <i>Đã từ chối</i>}</Box>
+      <Box>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>{request.state === "accepted" ? <i>Đã chấp nhận</i> : <i>Đã từ chối</i>}</Grid>
+          <Grid item>
+            {request.state === "accepted" ? (
+              <CheckIcon style={{ color: theme.palette.success.dark }}></CheckIcon>
+            ) : (
+              <CloseIcon style={{ color: theme.palette.error.dark }}></CloseIcon>
+            )}
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 }
