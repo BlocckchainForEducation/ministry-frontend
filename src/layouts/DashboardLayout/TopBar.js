c184e314e50ff80f6050323fdb6bd38142d33d6e
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-04 14:00:56 +0700

    :lock: update redux store: reset state when logout

--- src/layouts/DashboardLayout/TopBar.js
+++ src/layouts/DashboardLayout/TopBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import { AppBar, Badge, Box, Hidden, IconButton, Toolbar, makeStyles } from "@material-ui/core";
@@ -7,6 +7,10 @@ import MenuIcon from "@material-ui/icons/Menu";
 import NotificationsIcon from "@material-ui/icons/NotificationsOutlined";
 import InputIcon from "@material-ui/icons/Input";
 import Logo from "src/shared/utils/Logo";
+import { clearRole } from "../../utils/mng-role";
+import { clearToken } from "../../utils/mng-token";
+import { useDispatch } from "react-redux";
+import { resetStore } from "../../store";
 
 const useStyles = makeStyles(() => ({
   root: {},
@@ -19,6 +23,8 @@ const useStyles = makeStyles(() => ({
 const TopBar = ({ className, onMobileNavOpen, ...rest }) => {
   const classes = useStyles();
   const [notifications] = useState([]);
+  const navigate = useNavigate();
+  const dp = useDispatch();
 
   return (
     <AppBar className={clsx(classes.root, className)} elevation={0} {...rest}>
@@ -33,7 +39,15 @@ const TopBar = ({ className, onMobileNavOpen, ...rest }) => {
               <NotificationsIcon />
             </Badge>
           </IconButton>
-          <IconButton color="inherit">
+          <IconButton
+            color="inherit"
+            onClick={(e) => {
+              clearRole();
+              clearToken();
+              dp(resetStore());
+              navigate("/");
+            }}
+          >
             <InputIcon />
           </IconButton>
         </Hidden>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import votingReducer from "./views/Voting/redux";
 import uniListReducer from "./views/UniversityList/redux";
 import votedHistoryReducer from "./views/VoteHistory/redux";
+
+export const resetStore = () => {
+  return {
+    type: "RESET_STORE",
+  };
+};
+
+const rootReducer = (state, action) => {
+  if (action.type === "RESET_STORE") {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
+const appReducer = combineReducers({ votingSlice: votingReducer, universityListSlice: uniListReducer, voteHistorySlice: votedHistoryReducer });
+
 export default configureStore({
-  reducer: { votingSlice: votingReducer, universityListSlice: uniListReducer, voteHistorySlice: votedHistoryReducer },
+  reducer: rootReducer,
 });
